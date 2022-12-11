44f6e1d646c8e62d0f4a990245535edaef813b5d
경민 <a01098956117@gmail.com>
2022-12-11 18:32:35 +0900

    add login API

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,22 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import { AppDataSource } from './data-source';
 import authRoutes from './routes/auth';
 
 const app = express();
-const origin = 'http://localhost:3000';
-app.use(cors({ origin }));
+
+dotenv.config();
+
+const origin = process.env.ORIGIN;
+app.use(
+  cors({
+    origin,
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(morgan('dev'));
@@ -15,7 +24,7 @@ app.use(morgan('dev'));
 app.get('/', (_, res) => res.send('running'));
 app.use('/api/auth', authRoutes);
 
-let port = 4000;
+let port = process.env.PORT;
 
 app.listen(port, async () => {
   console.log(`Server running at http://localhost:${port}`);
