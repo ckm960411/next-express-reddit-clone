7f3c0d7ec93cbe2cf886771500ce0d26f0c48b9b
ckm960411 <a01098956117@gmail.com>
2022-12-12 22:13:26 +0900

    AuthContext 생성

---
+++ client/src/context/auth.tsx
@@ -0,0 +1,65 @@
+import { createContext, ReducerAction, useContext, useReducer } from 'react';
+import { User } from '../types';
+
+interface State {
+  authenticated: boolean;
+  user: User | undefined;
+  loading: boolean;
+}
+
+const StateContext = createContext<State>({
+  authenticated: false,
+  user: undefined,
+  loading: true,
+});
+
+const DispatchContext = createContext<any>(null);
+
+interface Action {
+  type: string;
+  payload: any;
+}
+const reducer = (state: State, { type, payload }: Action) => {
+  switch (type) {
+    case 'LOGIN':
+      return {
+        ...state,
+        authenticated: true,
+        user: payload.user,
+      };
+    case 'LOGOUT':
+      return {
+        ...state,
+        authenticated: false,
+        user: undefined,
+      };
+    case 'STOP_LOADING':
+      return {
+        ...state,
+        loading: false,
+      };
+    default:
+      throw new Error(`Unknown action type: ${type}`);
+  }
+};
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [state, defaultDispatch] = useReducer(reducer, {
+    user: undefined,
+    authenticated: false,
+    loading: true,
+  });
+
+  const dispatch = (type: string, payload?: any) => {
+    defaultDispatch({ type, payload });
+  };
+
+  return (
+    <DispatchContext.Provider value={dispatch}>
+      <StateContext.Provider value={state}>{children}</StateContext.Provider>
+    </DispatchContext.Provider>
+  );
+};
+
+export const useAuthState = () => useContext(StateContext);
+export const useAuthDispatch = () => useContext(DispatchContext);
