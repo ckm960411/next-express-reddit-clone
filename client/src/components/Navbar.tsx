b912055495ee0dae9acca1897a59f6ceadd00200
ckm960411 <a01098956117@gmail.com>
2022-12-25 18:55:20 +0900

    create Navbar component

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,56 @@
+import Link from 'next/link';
+import { useAuthState } from '../context/auth';
+
+const Navbar = () => {
+  const { authenticated, loading } = useAuthState();
+
+  const logout = () => {};
+
+  return (
+    <div className="fixed inset-x-0 top-0 z-10 flex items-center justify-between h-16 px-5 bg-white">
+      <span className="text-2xl font-semibold text-gray-400">
+        <Link href="/">Community</Link>
+      </span>
+
+      <div className="max-w-full px-4">
+        <div className="relative flex items-center bg-gray-100 border rounded hover:border-gray-700 hover:bg-white">
+          <i className="pl-4 pr-3 text-gray-400 fas fa-search"></i>
+          <input
+            type="text"
+            placeholder="Search"
+            className="px-3 py-1 bg-transparent rounded focus:outline-none"
+          />
+        </div>
+      </div>
+
+      <div className="flex">
+        {!loading &&
+          (authenticated ? (
+            <button
+              onClick={logout}
+              className="w-20 p-2 mr-2 te-xt-center text-white bg-gray-400 rounded"
+            >
+              Logout
+            </button>
+          ) : (
+            <>
+              <Link
+                href="/login"
+                className="w-20 p-2 mr-2 text-center text-blue-500 border border-blue-500 rounded"
+              >
+                log in
+              </Link>
+              <Link
+                href="/register"
+                className="w-20 p-2 mr-2 text-center text-white border bg-gray-400 rounded"
+              >
+                sign up
+              </Link>
+            </>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -2,14 +2,23 @@ import axios from 'axios';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { AuthProvider } from '../context/auth';
+import { useRouter } from 'next/router';
+import Navbar from '../components/Navbar';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { pathname } = useRouter();
+  const authRoutes = ['/register', '/login'];
+  const isAuthRoute = authRoutes.includes(pathname);
+
   axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + '/api';
   axios.defaults.withCredentials = true;
 
   return (
     <AuthProvider>
-      <Component {...pageProps} />
+      {!isAuthRoute && <Navbar />}
+      <div className={isAuthRoute ? '' : 'pt-12'}>
+        <Component {...pageProps} />
+      </div>
     </AuthProvider>
   );
 }
